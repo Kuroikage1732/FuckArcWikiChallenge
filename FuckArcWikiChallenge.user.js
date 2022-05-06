a3c04e436d3f4207c85c1f85617b86de6dfe6e67
Kuroikage1732 <62363347+Kuroikage1732@users.noreply.github.com>
2022-05-06 08:58:02 +0800

    Bug Fixed

--- FuckArcWikiChallenge.user.js
+++ FuckArcWikiChallenge.user.js
@@ -10,25 +10,23 @@
 // @require      https://unpkg.com/jquery@3.6.0/dist/jquery.min.js
 // ==/UserScript==
 this.$ = this.jQuery = jQuery.noConflict(true);
-$(function () {
-    console.log('FuckArcWikiChallenge Loaded');
-    if (document.getElementById('autohidden') != undefined) {
-        document.evaluate('/html/body/div[3]/div[3]/div[4]/div/table/tbody/tr/td[1]/div/img', document).iterateNext().src = "https://pic.rmb.bdstatic.com/bjh/3a01f3977ca05bb86115fe2fb144ba21.png";
-        console.debug('[FuckArcWikiChallenge]Replaced Visibility Icon');
-        $('#autohidden').html('');
-        console.debug('[FuckArcWikiChallenge]Cleared Question Div');
-        // $('#autoshow').removeAttr('class','test-hidden-2');
-        // console.debug('[FuckArcWikiChallenge]Showed Confirm Prompt');
-        $('div .light-blue').remove();
-        console.debug('[FuckArcWikiChallenge]Removed Confirm Prompt');
-        $('.box-技术型').remove()
-        console.debug('[FuckArcWikiChallenge]Removed Technical Warning');
-        $("p:first").attr('class', 'test-hidden');
-        console.debug('[FuckArcWikiChallenge]Hidden <p> Before Main Part');
-        $('.test-hidden').removeAttr('class', 'test-hidden');
-        console.debug('[FuckArcWikiChallenge]Showed Main Hidden Part');
-    }
-    else {
-        console.log('No element of id "autohidden" found. FuckArcWikiChallenge will ignore current page.');
-    }
-})()
+console.log('FuckArcWikiChallenge Loaded');
+if (document.getElementById('autohidden') != undefined) {
+    document.evaluate('/html/body/div[3]/div[3]/div[4]/div/table/tbody/tr/td[1]/div/img', document).iterateNext().src = "https://pic.rmb.bdstatic.com/bjh/3a01f3977ca05bb86115fe2fb144ba21.png";
+    console.debug('[FuckArcWikiChallenge]Replaced Visibility Icon');
+    $('#autohidden').html('');
+    console.debug('[FuckArcWikiChallenge]Cleared Question Div');
+    // $('#autoshow').removeAttr('class','test-hidden-2');
+    // console.debug('[FuckArcWikiChallenge]Showed Confirm Prompt');
+    $('div .light-blue').remove();
+    console.debug('[FuckArcWikiChallenge]Removed Confirm Prompt');
+    $('.box-技术型').remove()
+    console.debug('[FuckArcWikiChallenge]Removed Technical Warning');
+    $("p:first").css('display', 'none');
+    console.debug('[FuckArcWikiChallenge]Hidden <p> Before Main Part');
+    $('.test-hidden').removeAttr('class', 'test-hidden');
+    console.debug('[FuckArcWikiChallenge]Showed Main Hidden Part');
+}
+else {
+    console.log('No element of id "autohidden" found. FuckArcWikiChallenge will ignore current page.');
+}
