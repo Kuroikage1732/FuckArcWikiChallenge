15bf7a333b13b786e01e1af7a2eb5dedf0837819
Kuroikage1732 <62363347+Kuroikage1732@users.noreply.github.com>
2022-05-06 08:30:08 +0800

    Bug Fixed

--- FuckArcWikiChallenge.user.js
+++ FuckArcWikiChallenge.user.js
@@ -19,7 +19,7 @@ if (document.getElementById('autohidden') != undefined)
     console.debug('[FuckArcWikiChallenge]Cleared Question Div');
     // $('#autoshow').removeAttr('class','test-hidden-2');
     // console.debug('[FuckArcWikiChallenge]Showed Confirm Prompt');
-    $('<div> .light-blue').html('');
+    $('div .light-blue').html('');
     console.debug('[FuckArcWikiChallenge]Cleared Confirm Prompt');
     $('.test-hidden').removeAttr('class','test-hidden');
     console.debug('[FuckArcWikiChallenge]Showed Main Hidden Part');
