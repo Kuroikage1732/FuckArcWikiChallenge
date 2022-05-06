ccbb9ba34165ff4a1c1252830cd4f7e795c96138
Kuroikage1732 <62363347+Kuroikage1732@users.noreply.github.com>
2022-05-06 08:25:46 +0800

    Disable "Show Confirm Prompt"

--- FuckArcWikiChallenge.user.js
+++ FuckArcWikiChallenge.user.js
@@ -17,8 +17,8 @@ if (document.getElementById('autohidden') != undefined)
     console.debug('[FuckArcWikiChallenge]Replaced Visibility Icon');
     $('#autohidden').html('');
     console.debug('[FuckArcWikiChallenge]Cleared Question Div');
-    $('#autoshow').removeAttr('class','test-hidden-2');
-    console.debug('[FuckArcWikiChallenge]Showed Confirm Prompt');
+    // $('#autoshow').removeAttr('class','test-hidden-2');
+    // console.debug('[FuckArcWikiChallenge]Showed Confirm Prompt');
     $('.test-hidden').removeAttr('class','test-hidden');
     console.debug('[FuckArcWikiChallenge]Showed Main Hidden Part');
 }
